f9c76986256e31ef6de8add1e21672435f4450ef
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-13 13:06:45 +0100

    feat(ClassLevel/ClassLevelController.java): add RequestMapping annotation

--- src/main/java/com/springboot/EnableCorsClassMethodLevelSpringMVC/ClassLevel/ClassLevelController.java
+++ src/main/java/com/springboot/EnableCorsClassMethodLevelSpringMVC/ClassLevel/ClassLevelController.java
@@ -1,9 +1,11 @@
 package com.springboot.EnableCorsClassMethodLevelSpringMVC.ClassLevel;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
+@RequestMapping(path = "api/v1/cors")
 public class ClassLevelController {
 }
