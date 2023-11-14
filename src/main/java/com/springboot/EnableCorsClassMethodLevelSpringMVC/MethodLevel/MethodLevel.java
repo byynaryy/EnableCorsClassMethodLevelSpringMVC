04a38638a73c676dec639539af5b71ccf6750e1b
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-14 13:06:49 +0100

    feat(MethodLevel.java): add CrossOrigin annotation

--- src/main/java/com/springboot/EnableCorsClassMethodLevelSpringMVC/MethodLevel/MethodLevel.java
+++ src/main/java/com/springboot/EnableCorsClassMethodLevelSpringMVC/MethodLevel/MethodLevel.java
@@ -1,6 +1,7 @@
 package com.springboot.EnableCorsClassMethodLevelSpringMVC.MethodLevel;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api/v1/cors", produces = "text/plain")
 public class MethodLevel {
 
+    // Enable method level cors
+    @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
     @GetMapping("/method/level")
     public ResponseEntity<String> getPlainText() {
         // Logic to generate plain text
