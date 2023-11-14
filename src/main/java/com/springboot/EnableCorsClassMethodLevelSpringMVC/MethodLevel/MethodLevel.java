84d804161908245c2129dee76182419b96fea0b3
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-14 12:44:42 +0100

    feat(MethodLevel.java): add RestController annotation

--- src/main/java/com/springboot/EnableCorsClassMethodLevelSpringMVC/MethodLevel/MethodLevel.java
+++ src/main/java/com/springboot/EnableCorsClassMethodLevelSpringMVC/MethodLevel/MethodLevel.java
@@ -1,7 +1,9 @@
 package com.springboot.EnableCorsClassMethodLevelSpringMVC.MethodLevel;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping(path = "api/v1/cors", produces = "text/plain")
 public class MethodLevel {
 }
